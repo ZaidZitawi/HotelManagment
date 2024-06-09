ccb0ad539616cc96f9dbfd67c187790881b9ad6c
Zaid Zitawi <zaidzitawi879@gmail.com>
2024-06-09 19:12:27 +0100

    Initial commit

---
+++ src/test/java/com/example/HotelManagment/HotelManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HotelManagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
